299e987de9621c2ee99bc82a3bc50d30a70ac2b1
dileeph <dileep.hareendran@gmail.com>
2016-06-27 02:40:01 -0400

    added pdf,media,fixed maps in angular partials

--- src/main/java/com/maps/portal/MapSearchServiceApplication.java
+++ src/main/java/com/maps/portal/MapSearchServiceApplication.java
@@ -2,10 +2,16 @@ package com.maps.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MapSearchServiceApplication {
+public class MapSearchServiceApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MapSearchServiceApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(MapSearchServiceApplication.class, args);
 	}
